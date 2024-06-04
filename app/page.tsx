be0fa8a886a4d59b229f064accfaabd918d701b9
mohdshahzil <theshahzil@gmail.com>
2024-06-05 03:29:06 +0530

    svg added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
- 
 import { NavbarDemo } from "@/components/Navbar";
 import HumanSVG from "@/public/assets/SVG/HumanSVG";
 import React from "react";
@@ -11,8 +10,14 @@ const page = () => {
       <div className="h-5"></div>
       <div className="h-5"></div>
       <div className="h-5"></div>
-      <h1>This is the test</h1>
-     <HumanSVG></HumanSVG>
+      <div className="flex flex-col md:flex-row">
+        <div className="  ">
+          <h1></h1>
+        </div>
+        <div className="hidden md:block  absolute right-0 ">
+          <HumanSVG></HumanSVG>
+        </div>
+      </div>
     </div>
   );
 };
--- public/assets/SVG/HumanSVG.tsx
+++ public/assets/SVG/HumanSVG.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HumanSVG = () => {
   return (
-    <div>
+    <div className="  ">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="667.4107"
