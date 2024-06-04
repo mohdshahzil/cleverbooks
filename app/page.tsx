badc8f9a16382447381ddf9ecb3b0f0b271df774
mohdshahzil <theshahzil@gmail.com>
2024-06-05 03:07:44 +0530

    svg included

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
  
 import { NavbarDemo } from "@/components/Navbar";
+import HumanSVG from "@/public/assets/SVG/HumanSVG";
 import React from "react";
 
 const page = () => {
@@ -11,7 +12,7 @@ const page = () => {
       <div className="h-5"></div>
       <div className="h-5"></div>
       <h1>This is the test</h1>
-     
+     <HumanSVG></HumanSVG>
     </div>
   );
 };
